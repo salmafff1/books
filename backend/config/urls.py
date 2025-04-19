af5295b60aee88fb8fb3a7319235359efdbda59c
amal zramdini <88037785+amoulazrm@users.noreply.github.com>
2025-04-19 14:25:47 +0100

    django

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.http import HttpResponse
+from django.urls import path
+
+def home_view(request):
+    return HttpResponse("Bienvenue sur Book Review Platform !")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('books.urls')),
+    path('', home_view),  # Ajout d’une vue simple à la racine
 ]
